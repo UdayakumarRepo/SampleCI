c9118ed940e68568e8560a5f0da5d7df1aea3de7
Uday <udayakumarl@deemsysinc.com>
2020-04-30 17:27:19 +0530

    Added project files.

---
+++ SampleCI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleCI
+//
+//  Created by deemsys on 30/04/20.
+//  Copyright © 2020 deemsys. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
